9e8cb45336298a5f69585504a3ad006507bc1071
Divyesh Vanjare <vanjared@vmware.com>
2021-04-15 16:34:20 -0700

    Sample disk space logic implementation

--- gpMgmt/bin/lib/calculate_disk_free.py
+++ gpMgmt/bin/lib/calculate_disk_free.py
@@ -48,6 +48,28 @@ def _disk_free(directory):
 
     if directory == os.sep:
         return cmd
+    #importing the module 
+    import logging 
+    
+    #now we will Create and configure logger 
+    logging.basicConfig(filename="sunil_gpdb.log", 
+    					format='%(asctime)s %(message)s', 
+    					filemode='w') 
+    
+    #Let us Create an object 
+    logger=logging.getLogger() 
+    
+    #Now we are going to Set the threshold of logger to DEBUG 
+    logger.setLevel(logging.DEBUG) 
+    
+    #some messages to test
+    logger.debug("This is just a harmless debug message") 
+    logger.info("This is just an information for you") 
+    logger.warning("OOPS!!!Its a Warning") 
+    logger.error("Have you try to divide a number by zero") 
+    logger.critical("The Internet is not working....") 
+    
+    logger.error(" Return Value " + cmd.returncode)
 
     if cmd.returncode < 0:
         path, last_element = os.path.split(directory)
